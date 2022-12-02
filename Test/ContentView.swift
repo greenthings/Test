9cc5d43213e448e8afb5654e3794dc8bc0464a24
Jun Shin <mnhfn8hj@gmail.com>
2022-12-02 11:03:46 +0900

    third

--- Test/ContentView.swift
+++ Test/ContentView.swift
@@ -15,6 +15,7 @@ struct ContentView: View {
                 .foregroundColor(.accentColor)
             Text("Hello, world!")
             Text("Hello")
+            Text("Hello1")
         }
         .padding()
     }
